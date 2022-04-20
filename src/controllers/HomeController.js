1a9a52ece0b79e275c30281544d792547b32242f
Guilherme Almeida <guimathiasgui@gmail.com>
2022-04-20 20:50:48 -0300

    Setting and testing Sequelize

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,8 +1,16 @@
+import Student from '../models/Student';
+
 class HomeController {
-  index(req, res) {
-    res.json({
-      ok: true,
+  async index(req, res) {
+    const newStudent = await Student.create({
+      name: 'Guilherme',
+      surname: 'Mathias',
+      email: 'guilherme@gmail.com',
+      age: 22,
+      weight: 60,
+      height: 1.70,
     });
+    res.json(newStudent);
   }
 }
 
