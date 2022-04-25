3b52edbd7869526ab0855af18cb8666899cb054d
Guilherme Almeida <guimathiasgui@gmail.com>
2022-04-25 20:59:52 -0300

    Creating seeds

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,6 +4,8 @@ import loginRequired from '../middlewares/loginRequired';
 
 const router = new Router();
 
+router.get('/', userController.index);
+
 router.post('/', userController.store);
 router.put('/', loginRequired, userController.update);
 router.delete('/', loginRequired, userController.delete);
