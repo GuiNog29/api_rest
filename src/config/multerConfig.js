fc7be280f3325cdf29c2380c16884cb8e4b862e4
Guilherme Almeida <guimathiasgui@gmail.com>
2022-04-28 21:47:34 -0300

    Treating uploaded files

--- src/config/multerConfig.js
+++ src/config/multerConfig.js
@@ -4,6 +4,14 @@ import { extname, resolve } from 'path';
 const random = () => Math.floor(Math.random() * 10000 + 10000);
 
 export default {
+  fileFilter: (req, file, callback) => {
+    if (file.mimetype !== 'image/png' && file.mimetype !== 'image/jpeg') {
+      return callback(new multer.MulterError('File must be PNG or JPEG'));
+    }
+
+    return callback(null, true);
+  },
+
   storage: multer.diskStorage({
     destination: (req, file, callBack) => {
       callBack(null, resolve(__dirname, '..', '..', 'uploads'));
--- src/routes/pictureRoutes.js
+++ src/routes/pictureRoutes.js
@@ -1,12 +1,8 @@
 import { Router } from 'express';
-import multer from 'multer';
 import pictureController from '../controllers/PictureController';
-import multerConfig from '../config/multerConfig';
-
-const upload = multer(multerConfig);
 
 const router = new Router();
 
-router.post('/', upload.single('picture'), pictureController.store);
+router.post('/', pictureController.store);
 
 export default router;
