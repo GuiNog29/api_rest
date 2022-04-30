83b6a5de6b0984bd649751bb8abe26e3d70458d2
Guilherme Almeida <guimathiasgui@gmail.com>
2022-04-30 20:50:02 -0300

    Saving picture in database

--- src/controllers/PictureController.js
+++ src/controllers/PictureController.js
@@ -1,18 +1,23 @@
 import multer from 'multer';
 import multerConfig from '../config/multerConfig';
+import Picture from '../models/Picture';
 
 const upload = multer(multerConfig).single('picture');
 
 class PictureController {
-  async store(req, res) {
-    return upload(req, res, (error) => {
+  store(req, res) {
+    return upload(req, res, async (error) => {
       if (error) {
         return res.status(400).json({
           errors: [error.code],
         });
       }
 
-      return res.json(req.file);
+      const { originalname, filename } = req.file;
+      const { student_id } = req.body;
+      const picture = await Picture.create({ originalname, filename, student_id });
+
+      return res.json(picture);
     });
   }
 }
