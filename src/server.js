b968a4bebbdf9768abb8cbc1a34d19e2b76f25de
Guilherme Almeida <guimathiasgui@gmail.com>
2022-05-16 22:09:36 -0300

    Reconfiguring API to run local

--- src/server.js
+++ src/server.js
@@ -1,4 +1,7 @@
 import app from './app';
 
 const port = process.env.APP_PORT;
-app.listen(port);
+app.listen(port, () => {
+  console.log();
+  console.log(`CTRL + Click in http://localhost:${port}`);
+})
